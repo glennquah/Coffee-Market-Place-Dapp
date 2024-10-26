73020903a5c08e02f879bd199f53ba7f0da0b02f
Glenn Quah <quah.glenn@gmail.com>
2024-10-26 17:32:33 +0800

    feat: add base navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
+// src/App.tsx
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Marketplace from './pages/MarketPlace';
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Navbar />
+      <div className="pt-24">
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/marketplace" element={<Marketplace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/pages/MarketPlace.tsx
@@ -0,0 +1,70 @@
+function Marketplace() {
+  return (
+    <div>
+      <h1>Marketplace</h1>
+      <div style={{ display: 'flex', gap: '20px' }}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dolore
+          voluptatibus eum odio amet nihil ad, veritatis eius deleniti quae
+          aperiam asperiores autem voluptatem eligendi quibusdam rerum earum
+          exercitationem non ducimus esse aut ab, ullam nulla accusantium.
+          Maiores voluptates soluta, ipsum vero eaque doloribus tenetur est
+          dolorum et officiis ipsam quisquam necessitatibus reprehenderit ut
+          modi, delectus veritatis magni. Veritatis blanditiis cupiditate
+          reprehenderit ducimus, quis libero. Facilis vero, temporibus
+          laboriosam blanditiis repellendus dolores ullam reprehenderit tempore
+          at sequi illo obcaecati, dolore maxime reiciendis? Sit culpa vitae
+          officia eos quisquam explicabo dolor adipisci ab neque facere,
+          laboriosam repudiandae eligendi ullam velit cum odio soluta, veniam
+          iusto. Sunt voluptatibus eius amet minus, sapiente est minima
+          obcaecati blanditiis iste dolorum odio, commodi quas temporibus ipsum
+          sequi? Ex, voluptatibus! Placeat quis distinctio amet illo cupiditate
+          ut blanditiis repellat facilis laudantium culpa! Molestiae accusantium
+          quia ducimus modi nisi, eaque fuga beatae fugiat voluptatibus eius
+          soluta placeat error nihil eligendi aut consequuntur harum. Minus
+          temporibus libero impedit quasi corrupti qui quos laborum. Sunt
+          cupiditate itaque a quaerat laboriosam accusantium porro minus,
+          excepturi explicabo. Error, a deleniti. Laborum natus a aliquid omnis
+          quaerat? Placeat, esse numquam soluta totam cumque incidunt eius
+          debitis nihil ipsa impedit reiciendis? Officia dolor hic soluta
+          exercitationem aspernatur. Enim deleniti qui vel repellat doloremque
+          mollitia tenetur illum atque ut pariatur? Laudantium labore nobis,
+          blanditiis suscipit id sit, eligendi est voluptatem, aliquid incidunt
+          beatae consequatur officia? Dolorem consectetur modi, hic aut
+          inventore eius, esse aliquid aspernatur doloribus molestiae
+          praesentium vitae voluptatum sit! In, dolor accusantium! Doloremque
+          veniam expedita repellat non quibusdam eaque facilis iusto maiores,
+          fuga, magnam quos, repellendus quo ea pariatur voluptatibus deserunt!
+          Minus cupiditate, libero vel itaque, veniam dignissimos commodi
+          perferendis nisi quas, rerum ipsam dolor dolores beatae. Molestiae
+          dicta amet id alias necessitatibus qui fugiat est dolorem nulla
+          repellat! Dolorum, dicta! Molestiae sequi architecto cum eaque, labore
+          non odit repellendus hic ducimus, excepturi sed? Debitis cum, illo
+          recusandae doloribus nobis odio magni commodi soluta et saepe
+          voluptatum dolores aliquam a praesentium laudantium voluptate,
+          adipisci expedita dolorum! Fugit ipsa necessitatibus vel nostrum
+          maxime molestias, fuga ipsum laboriosam quas quod sit distinctio
+          laborum omnis? Rerum maiores earum debitis dolorem quae, labore
+          doloremque aut repellat, quidem blanditiis quod, minus sequi porro
+          harum vitae ab? Tenetur at ab explicabo fugiat et itaque inventore
+          sequi laborum quidem ipsa velit in rem omnis, odit eum praesentium
+          ipsum recusandae corrupti doloribus, eos perferendis repudiandae
+          harum. Vel, beatae itaque? Officia odit ab voluptatum sunt eaque
+          perspiciatis quis accusamus ea dicta ut eius labore reiciendis, qui
+          quisquam reprehenderit soluta praesentium explicabo perferendis dolore
+          maiores sint ratione! Saepe corporis tempore voluptatibus atque dicta
+          recusandae, odio voluptas repellendus ipsam obcaecati praesentium
+          rerum molestias. Dolorem, non? Quibusdam nisi blanditiis aliquid
+          nesciunt eius ab veniam magnam laborum cum architecto placeat alias
+          corporis officia necessitatibus sed eum perferendis excepturi, error
+          quo ut iure! Laudantium suscipit magni illo molestias. Ratione nostrum
+          minima sapiente possimus itaque quod sunt, quae aliquam. Sint vitae
+          id, obcaecati est voluptate dolorem culpa odit maiores, impedit porro
+          placeat accusamus optio, tenetur molestiae quos.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Marketplace;
