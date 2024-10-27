ecc0183239780e49948b032882424459098af1cb
Glenn Quah <quah.glenn@gmail.com>
2024-10-27 15:09:23 +0800

    feat: add hero to shopping cart page

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -1,17 +1,16 @@
+import Hero from '../components/MainHero';
 import ResponsiveContainer from '../components/ResponsiveContainer';
+import { HeroVariant } from '../types/types';
 
 function CartPage() {
   return (
     <div className="py-8">
       <ResponsiveContainer>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-          voluptas nemo ea praesentium dolor rem explicabo omnis inventore
-          laborum deleniti velit unde, vitae quas quidem ab non, tempora officia
-          repellendus quia culpa. Architecto molestiae facere hic laboriosam
-          velit magni modi odio earum impedit distinctio inventore laborum,
-          ipsam repellendus, fuga dolor.
-        </p>
+        <Hero
+          variant={HeroVariant.V1}
+          title={'Shopping Cart'}
+          subTitle={'Review your items and proceed to checkout.'}
+        />
       </ResponsiveContainer>
     </div>
   );
