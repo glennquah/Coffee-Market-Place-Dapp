1cc0e10594496dec5345e8fe93df80cbb84882a2
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2024-10-28 23:49:22 +0800

    Refactor CoffeeAuction.test.ts to include additional test cases for bid committing and revealing

--- smart_contract/test/CoffeeAuction.test.ts
+++ smart_contract/test/CoffeeAuction.test.ts
@@ -108,9 +108,59 @@ describe('Coffee Auction E2E Test', function () {
         )
     ).to.emit(coffeeAuction, 'BidCommitted');
   });
+
+  it("should revert if bidder commits a bid with invalid auction ID", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          2, 
+          hashValue1
+        )
+    ).to.be.revertedWith('Auction does not exist');
+  });
+
+  it("should revert if bidder commits a bid outside of commit phrase", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          1, 
+          hashValue1
+        )
+    ).to.be.revertedWith('Commit phase has ended');
+  });
   
   it("should allow bidder to reveal a bid", async function() {
-     // Auctioneer create a new auction
+    // Auctioneer create a new auction
     await expect(
       coffeeAuction
       .connect(roaster)
@@ -144,6 +194,8 @@ describe('Coffee Auction E2E Test', function () {
     ).to.emit(coffeeAuction, 'BidCommitted');
 
     // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
     await expect(
       coffeeAuction
         .connect(customer1)
@@ -165,6 +217,78 @@ describe('Coffee Auction E2E Test', function () {
           {from:customer2.getAddress(),value:bidAmount2}
         )
     ).to.emit(coffeeAuction,'BidRevealed');
+  });
+
+  it("should revert if bidder reveals a bid with invalid auction ID", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          1, 
+          hashValue1
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+    // Bidders reveal bids
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .revealBid(
+          2,
+          1,
+          123,
+          {from:customer1.getAddress(),value:bidAmount1}
+        )
+    ).to.be.revertedWith('Auction does not exist');
+  });
+
+  it("should revert if bidder reveals a bid outside of reveal phrase", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          1, 
+          hashValue1
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+    // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [180 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .revealBid(
+          1,
+          1,
+          123,
+          {from:customer1.getAddress(),value:bidAmount1}
+        )
+    ).to.be.revertedWith('Not within reveal phase');
   });
 
   it("should allow auctioneer to finalize auction", async function() {
@@ -202,6 +326,217 @@ describe('Coffee Auction E2E Test', function () {
     ).to.emit(coffeeAuction, 'BidCommitted');
 
     // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .revealBid(
+          1,
+          1,
+          123,
+          {from:customer1.getAddress(),value:bidAmount1}
+        )
+    ).to.emit(coffeeAuction,'BidRevealed');
+
+    await expect(
+      coffeeAuction
+        .connect(customer2)
+        .revealBid(
+          1,
+          12,
+          123,
+          {from:customer2.getAddress(),value:bidAmount2}
+        )
+    ).to.emit(coffeeAuction,'BidRevealed');
+
+    // Auctioneer finalize auction
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(roaster)
+        .finalizeAuction(1)
+    ).to.emit(coffeeAuction,'AuctionFinalized')
+    .to.emit(coffeeAuction,"NFTTransferred")
+    .to.emit(coffeeAuction,"HighestBidTransferred");
+  });
+
+  it("should revert if non-owner or non-auctioneer finalizes auction", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    const hashValue2 = await hash.hash(12,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          1, 
+          hashValue1
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+
+    await expect(
+      coffeeAuction
+        .connect(customer2)
+        .commitBid(
+          1, 
+          hashValue2
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+
+    // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .revealBid(
+          1,
+          1,
+          123,
+          {from:customer1.getAddress(),value:bidAmount1}
+        )
+    ).to.emit(coffeeAuction,'BidRevealed');
+
+    await expect(
+      coffeeAuction
+        .connect(customer2)
+        .revealBid(
+          1,
+          12,
+          123,
+          {from:customer2.getAddress(),value:bidAmount2}
+        )
+    ).to.emit(coffeeAuction,'BidRevealed');
+
+    // Auctioneer finalize auction
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .finalizeAuction(1)
+    ).to.revertedWith('Only seller and owner can call this');
+  });
+
+  it("should revert if auctioneer finalizes auction with invalid auction ID", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    const hashValue2 = await hash.hash(12,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          1, 
+          hashValue1
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+
+    await expect(
+      coffeeAuction
+        .connect(customer2)
+        .commitBid(
+          1, 
+          hashValue2
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+
+    // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .revealBid(
+          1,
+          1,
+          123,
+          {from:customer1.getAddress(),value:bidAmount1}
+        )
+    ).to.emit(coffeeAuction,'BidRevealed');
+
+    await expect(
+      coffeeAuction
+        .connect(customer2)
+        .revealBid(
+          1,
+          12,
+          123,
+          {from:customer2.getAddress(),value:bidAmount2}
+        )
+    ).to.emit(coffeeAuction,'BidRevealed');
+
+    // Auctioneer finalize auction
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
+    await expect(
+      coffeeAuction
+        .connect(owner)
+        .finalizeAuction(2)
+    ).to.revertedWith('Auction does not exist');
+  });
+
+  it("should revert if auctioneer finalizes auction outside of finalize phrase", async function() {
+    // Auctioneer create a new auction
+    await expect(
+      coffeeAuction
+      .connect(roaster)
+      .createAuction(
+        1,
+        1,
+        2,
+        {from:roaster.getAddress(),value:auctionFee}
+      )
+    ).to.emit(coffeeAuction, 'AuctionCreated');
+    
+    // Bidders commit bids
+    const hashValue1 = await hash.hash(1,123);
+    const hashValue2 = await hash.hash(12,123);
+    await expect(
+      coffeeAuction
+        .connect(customer1)
+        .commitBid(
+          1, 
+          hashValue1
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+
+    await expect(
+      coffeeAuction
+        .connect(customer2)
+        .commitBid(
+          1, 
+          hashValue2
+        )
+    ).to.emit(coffeeAuction, 'BidCommitted');
+
+    // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
     await expect(
       coffeeAuction
         .connect(customer1)
@@ -229,9 +564,7 @@ describe('Coffee Auction E2E Test', function () {
       coffeeAuction
         .connect(roaster)
         .finalizeAuction(1)
-    ).to.emit(coffeeAuction,'AuctionFinalized')
-    .to.emit(coffeeAuction,"NFTTransferred")
-    .to.emit(coffeeAuction,"HighestBidTransferred");
+    ).to.revertedWith('Reveal phase still ongoing');
   });
 
   it("should allow non-winning bidder to withdraw funds", async function() {
@@ -269,6 +602,8 @@ describe('Coffee Auction E2E Test', function () {
     ).to.emit(coffeeAuction, 'BidCommitted');
 
     // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
     await expect(
       coffeeAuction
         .connect(customer1)
@@ -292,6 +627,8 @@ describe('Coffee Auction E2E Test', function () {
     ).to.emit(coffeeAuction,'BidRevealed');
 
     // Auctioneer finalize auction
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
     await expect(
       coffeeAuction
         .connect(roaster)
@@ -343,6 +680,8 @@ describe('Coffee Auction E2E Test', function () {
     ).to.emit(coffeeAuction, 'BidCommitted');
 
     // Bidders reveal bids
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
     await expect(
       coffeeAuction
         .connect(customer1)
@@ -366,6 +705,8 @@ describe('Coffee Auction E2E Test', function () {
     ).to.emit(coffeeAuction,'BidRevealed');
 
     // Auctioneer finalize auction
+    await ethers.provider.send('evm_increaseTime', [90 * 60]);
+    await ethers.provider.send('evm_mine');
     await expect(
       coffeeAuction
         .connect(roaster)
