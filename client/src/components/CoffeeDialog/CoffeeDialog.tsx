b1090df104fa44c2e4b4b16fd560d92e70ed9bf4
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2024-10-28 14:37:09 +0800

    update className

--- client/src/components/CoffeeDialog/CoffeeDialog.tsx
+++ client/src/components/CoffeeDialog/CoffeeDialog.tsx
@@ -53,8 +53,8 @@ const CoffeeDialog = () => {
                     <h1 style={{fontSize:20,fontWeight:"bold"}}>Upload your coffee into an NFT ☕</h1>
                     <h1 style={{fontSize:20,fontWeight:"bold"}}>Let’s tokenise your coffee into NFTs!</h1>
                 </div>
-                <div className="container">
-                    <div className='inner-container'>
+                <div className="containers">
+                    <div className='inner-containers'>
                         <div style={{width:300,height:300}}>
                             <img src={image} style={{width:300,height:300}}/>
                         </div>
@@ -64,7 +64,7 @@ const CoffeeDialog = () => {
                             accept="application/png" onChange={onImageChange}></input>
                         </div>
                     </div>
-                    <div className='inner-container'>
+                    <div className='inner-containers'>
                         <div style={{width:"100%",display:"flex",justifyContent:"space-between",marginTop:10,marginBottom:10}}>
                             <label style={{fontSize:20}}>Coffee Name:</label>
                             <input type="text" style={{width:250,borderRadius:5,color:"black",padding:5}} 
